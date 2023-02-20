852aa0f1cb4828c231ab14303810a93071fdf1c5
Julio Nabor <julionabor@gmail.com>
2023-02-20 09:09:41 +0000

    new changes

--- js/script.js
+++ js/script.js
@@ -1,131 +1,122 @@
-let rotation = 0
-let question = 0
-let recommendation = document.getElementById('recommendation')
-let message= document.getElementById("message")
-if (rotation === 0) { 
-  //Recommendation 1
-  recommendation.innerHTML = `
-  ____________________________________________<br/>
-  <h2>RECOMMENDATION:</h2>
-  For depression screening in PWE, the
-  Neurological Disorders Depression
-  Inventory for Epilepsy (NDDI-E) with a cutoff
-  point of >13 is recommended.`
-  question= 1
-}
-function rotateImg(param) {
-  message.innerHTML= ''
-  if(param == 1){
-    rotation += -24; // avançar
-    question = question+1;
-    console.log("+")
-  }
-  if(param == 0){ 
-    rotation +=24; // voltar
-    question= question-1;
-    console.log("-")
-  }
-  console.log(question)
-  if (rotation === -360) { 
-    // 360 means rotate back to 0
-    container.style.backgroundColor = '#00BFFF';
-    rotation = 0;
-    question=1
-    //Recommendation 1
-  recommendation.innerHTML = `
-  ____________________________________________<br/>
-  <h2>RECOMMENDATION:</h2>
-  For depression screening in PWE, the
-  Neurological Disorders Depression
-  Inventory for Epilepsy (NDDI-E) with a cutoff
-  point of >13 is recommended.`
-  
-  }
-
-  if(rotation == -24){
-    //Recommendation 2
-    recommendation.innerHTML = `
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+const slot = [
+  {
+    index: 0,
+    angle: 0,
+    recommendation: `<h2>RECOMMENDATION:</h2>
+    For depression screening in PWE, the
+    Neurological Disorders Depression
+    Inventory for Epilepsy (NDDI-E) with a cutoff
+    point of >13 is recommended.`,
+    question: `<h2> QUESTION </h2>
+    What screening tool for depression should
+    be used for PWE?`
+  },
+  {
+    index: 1,
+    angle: 35,
+    recommendation: `<h2>RECOMMENDATION</h2>
     In PWE, it is recommended that a
     neurologist and/or psychiatrist make a
     diagnosis of depression through anamnesis
-    and following the ICD-10 diagnostic criteria.`
-   
-  }else if(rotation == -48){
-    //Recommendation 3
-    recommendation.innerHTML = `
-     ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    and following the ICD-10 diagnostic criteria.`,
+    question: `<h2> QUESTION </h2>
+    How should depression be
+    diagnosed in PWE?`
+  },
+  {
+    index: 2,
+    angle: 58,
+    recommendation: `<h2>RECOMMENDATION</h2>
     In PWE, it is recommended to evaluate the
     risk of suicide by their neurologist and/or
-    psychiatrist through anamnesis.`
-    
-  } else if(rotation == -72){
-    //Recommendation 4
-    recommendation.innerHTML = `
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    psychiatrist through anamnesis.`,
+    question: `<h2> QUESTION </h2>
+    How should the risk of suicide
+    be assessed in PWE?`
+  },
+  {
+    index: 3,
+    angle: 80,
+    recommendation: `<h2>RECOMMENDATION</h2>
     Depression is considered to be secondary to
     epilepsy in those patients in whom
     depressive symptoms occur with seizures
     (peri-ictal) or when they are related to
-    antiseizure medication.`
-    
-  } else if(rotation == -96){
-    //Recommendation 5
-    recommendation.innerHTML = `
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    antiseizure medication.`,
+    question: `<h2> QUESTION </h2>
+    In what situations do you
+    consider that depression is
+    secondary to epilepsy?`
+  },
+  {
+    index: 4,
+    angle: 102,
+    recommendation: `<h2>RECOMMENDATION</h2>
     In PWE, it is recommended to refer the patient to a
     psychiatrist as soon as possible if there is suspicion of
     another psychiatric comorbidity, psychotic depression,
     bipolar disorder, risk of suicide, if there is no response
     to the second antidepressant treatment (at adequate
     dose, duration and adherence) or if augmentation
-    of antidepressant treatment is needed..`
-    container.style.backgroundColor = '#9400D3';
-    
-  } else if(rotation == -120){
-    //Recommendation 6
-    recommendation.innerHTML = `
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    of antidepressant treatment is needed..`,
+    question: `<h2> QUESTION </h2>
+    A. What are the situations in which referral to
+    psychiatry should not be delayed in a PWE and
+    depression?
+    B. After how many therapeutic attempts, with
+    adequate dosage and duration, is it recommended to
+    refer the patient to psychiatry?`
+  },
+  {
+    index: 5,
+    angle: 124,
+    recommendation: `<h2>RECOMMENDATION</h2>
     In PWE and depression, the selection of antiseizure
     medication will depend on the characteristics of the disease
     and the patient's profile. In general, the use of lamotrigine,
     valproate, eslicarbazepine acetate, carbamazepine, and
     oxcarbazepine is recommended; the use of
     levetiracetam, phenobarbital, topiramate, and
-    perampanel is less recommended.`
-    
-  }else if(rotation == -144){
-    //Recommendation 7
-    recommendation.innerHTML = `
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    perampanel is less recommended.`,
+    question: `<h2> QUESTION </h2>
+    In PWE and depression:
+    A. What is the recommended antiseizure
+    medication?
+    B. What antiseizure medications should
+    be avoided?`
+  },
+  {
+    index: 6,
+    angle: 146,
+    recommendation: `<h2>RECOMMENDATION</h2>
     In PWE and depression, it is recommended to start
     treatment for depression as soon as possible after
-    diagnosis, especially if there is a risk of suicide.`
-    
-  }else if(rotation == -168){
-    //Recommendation 8
-    recommendation.innerHTML = `    
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    diagnosis, especially if there is a risk of suicide.`,
+    question: `<h2> QUESTION </h2>
+    When should antidepressant
+    treatment be started in PWE?`
+  },
+  {
+    index: 7,
+    angle: 170,
+    recommendation: `<h2>RECOMMENDATION</h2>
     In PWE and depression, antidepressant treatment
     selection will depend on the characteristics of the
     disease and the patient's profile. In general, the
     use of selective serotonin reuptake inhibitors
     and dual-type antidepressants is
     recommended; bupropion, clomipramine,
-    and maprotiline are less recommended.`
-    
-  }else if(rotation == -192){
-    //Recommendation 9
-    recommendation.innerHTML = `    
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    and maprotiline are less recommended.`,
+    question: `<h2> QUESTION </h2>
+    In PWE and depression:
+    A. What is the recommended antidepressant
+    medication?
+    B. What antidepressants should be avoided?`
+  },
+  {
+    index: 8,
+    angle: 192,
+    recommendation: `<h2>RECOMMENDATION</h2>
     In pregnant PWE and depression, a risk-benefit balance
     should be made between using antidepressants and not
     using them. The patient should be informed of that
@@ -135,13 +126,17 @@ function rotateImg(param) {
     clinical chart. If an antidepressant is needed,
     SSRIs and SNRIs are usually the
     first-choice treatment for this
-    population.`
-    
-  }else if(rotation == -216){
-    //Recommendation 10
-    recommendation.innerHTML = `    
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    population.`,
+    question: `<h2> QUESTION </h2>
+    In a pregnant patient with epilepsy and
+    depression:
+    A. What is the recommended antidepressant medication?
+    B. What antidepressants should be avoided?`
+  },
+  {
+    index: 9,
+    angle: 216,
+    recommendation: `<h2>RECOMMENDATION</h2>
     In PWE and mild depressive episodes, psychotherapy is
     recommended, and if the depressive episodes are
     moderate-severe, psychotherapy in combination with
@@ -149,171 +144,152 @@ function rotateImg(param) {
     case, the psychotherapist will establish the
     number of psychotherapy sessions depending
     on the patient and the type of therapy
-    established.`
-    
-  }else if(rotation == -240){
-    //Recommendation 11
-    recommendation.innerHTML = `    
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    established.`,
+    question: `<h2> QUESTION </h2>
+    A. When should psychotherapy be recommended for
+    a patient with epilepsy and depression?
+    B. What is the recommended number of psychotherapy
+    sessions?`
+  },
+  {
+    index: 10,
+    angle: 238,
+    recommendation: `<h2>RECOMMENDATION</h2>
     It is recommended to continue antidepressant
     treatment for a minimum of 6 months after
     remission in patients with a first episode and a
     minimum of 9 months in cases of a recurrent
-    episode or severe depression.`
-    
-  }else if(rotation == -264){
-    //Recommendation 12
-    recommendation.innerHTML = `    
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    episode or severe depression.`,
+    question: `<h2> QUESTION </h2>
+    How long should antidepressant treatment be
+    maintained in PWE?`
+  },
+  {
+    index: 11,
+    angle: 260,
+    recommendation: `<h2>RECOMMENDATION</h2>
     When an antidepressant should be discontinued, it is
     recommended to do so gradually over 1 to 4
-    weeks.`
-    
-  }else if(rotation == -288){
-    //Recommendation 13
-    recommendation.innerHTML = `    
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    weeks.`,
+    question: `<h2> QUESTION </h2>
+    How should antidepressant treatment be
+    discontinued in PWE?`
+  },
+  {
+    index: 12,
+    angle: 282,
+    recommendation: `<h2>RECOMMENDATION</h2>
     It is suggested to change ASM in a PWE diagnosed with
     depression if a causal relationship between the ASM and
     the depressive episode is suspected or if the
     depressive symptoms may worsen due to the ASM
-    treatment.`
-   
-  }else if(rotation == -312){
-    //Recommendation 14
-    recommendation.innerHTML = `    
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    treatment.`,
+    question: `<h2> QUESTION </h2>
+    Is it recommended to change antiseizure
+    medications in PWE with a diagnosis of depression?
+    In what cases?
+    For which medications?` 
+  },
+  {
+    index: 13,
+    angle: 304,
+    recommendation: `<h2>RECOMMENDATION</h2>
     Follow-up of PWE and depression must be maintained
     according to the specialist's judgment, generally
     recommending follow-up for at least 6-12 months after
     remission of depressive symptoms and withdrawal
-    of treatment.`
-    container.style.backgroundColor = '#191970';
-   
-  }else if(rotation == -336){
-    //Recommendation 15
-    recommendation.innerHTML = `    
-    ____________________________________________<br/>
-    <h2>RECOMMENDATION</h2>
+    of treatment.`,
+    question: `<h2> QUESTION </h2>
+    How long should a psychiatric follow-up
+    of a patient with epilepsy and depression
+    be maintained?`
+  },
+  {
+    index: 14,
+    angle: 326,
+    recommendation: `<h2>RECOMMENDATION</h2>
     The psychiatric follow-up of a PWE at risk of
     suicide must be maintained according to the
     psychiatrist's judgment, following the
     recommendations and/or follow-up clinical
-    protocols established at their center.`
-    
-  }
-  document.querySelector("#img").style.transform = `rotate(${rotation}deg)`;
-}
-  let element = document.getElementById("question")
-  element.addEventListener("click", showQuestion);
+    protocols established at their center.`,
+    question: `<h2> QUESTION </h2>
+    How long should a psychiatric follow-up
+    of a patient with epilepsy and a risk of
+    suicide be maintained?`
+  },
   
-function showQuestion(){
-  if(question <= 0){
-    question= 15;
-  }
-  if(question > 15){
-    question= 1;
-  }
-  switch(question){
-    case 1: 
-      message.innerHTML = `<h2> QUESTION </h2>
-      What screening tool for depression should
-      be used for PWE?`
-      break
-    case 2:
-      message.innerHTML = `<h2> QUESTION </h2>
-      How should depression be
-      diagnosed in PWE?`
-      break
-    case 3:
-      message.innerHTML = `<h2> QUESTION </h2>
-      How should the risk of suicide
-      be assessed in PWE?`
-      break
-    case 4:
-      message.innerHTML = `<h2> QUESTION </h2>
-      In what situations do you
-      consider that depression is
-      secondary to epilepsy?`
-      break
-    case 5:
-      message.innerHTML = `<h2> QUESTION </h2>
-      A. What are the situations in which referral to
-      psychiatry should not be delayed in a PWE and
-      depression?
-      B. After how many therapeutic attempts, with
-      adequate dosage and duration, is it recommended to
-      refer the patient to psychiatry?`
-      break
-    case 6:
-      message.innerHTML = `<h2> QUESTION </h2>
-      In PWE and depression:
-      A. What is the recommended antiseizure
-      medication?
-      B. What antiseizure medications should
-      be avoided?`
-      break
-    case 7:
-      message.innerHTML = `<h2> QUESTION </h2>
-      When should antidepressant
-      treatment be started in PWE?`
-      break
-    case 8:
-      message.innerHTML = `<h2> QUESTION </h2>
-      In PWE and depression:
-      A. What is the recommended antidepressant
-      medication?
-      B. What antidepressants should be avoided?`
-      break
-    case 9:
-      message.innerHTML = `<h2> QUESTION </h2>
-      In a pregnant patient with epilepsy and
-      depression:
-      A. What is the recommended antidepressant medication?
-      B. What antidepressants should be avoided?`
-      break
-    case 10:
-      message.innerHTML = `<h2> QUESTION </h2>
-      A. When should psychotherapy be recommended for
-      a patient with epilepsy and depression?
-      B. What is the recommended number of psychotherapy
-      sessions?`
-      break
-    case 11:
-      message.innerHTML = `<h2> QUESTION </h2>
-      How long should antidepressant treatment be
-      maintained in PWE?`
-      break
-    case 12:
-      message.innerHTML = `<h2> QUESTION </h2>
-      How should antidepressant treatment be
-      discontinued in PWE?`
-      break
-    case 13:
-      message.innerHTML = `<h2> QUESTION </h2>
-      Is it recommended to change antiseizure
-      medications in PWE with a diagnosis of depression?
-      In what cases?
-      For which medications?`      
-      break
-    case 14:
-      message.innerHTML = `<h2> QUESTION </h2>
-      How long should a psychiatric follow-up
-      of a patient with epilepsy and depression
-      be maintained?`
-      break
-    case 15:
-      message.innerHTML = `<h2> QUESTION </h2>
-      How long should a psychiatric follow-up
-      of a patient with epilepsy and a risk of
-      suicide be maintained?`
-      break
-    default:
-      message.innerHTML= ''
+]
+
+const rec = document.getElementById('recommendation')
+const message= document.getElementById("message")
+const img = document.querySelector("#img")
+const element = document.getElementById("question")
+const container = document.getElementById("container")
+
+  //start
+  let index =slot[0].index
+  rec.innerHTML = `____________________________________________<br/>`
+  rec.innerHTML += slot[index].recommendation
+  element.addEventListener("click", ()=> message.innerHTML = slot[index].question);
+
+function goToNext(){
+  rec.innerHTML= ''
+  message.innerHTML=''
+  if(index >= (slot.length - 1)){
+    index= 0
+    img.style.transform = `rotate(-${slot[index].angle}deg)`;
+    img.style.transition = "all 1s";
     
+
+    rec.innerHTML = `____________________________________________<br/>`
+    rec.innerHTML += slot[index].recommendation
+    element.addEventListener("click", ()=> message.innerHTML = slot[index].question);
   }
+  else{ 
+    index= index+1
+    img.style.transform = `rotate(-${slot[index].angle}deg)`;
+    img.style.transition = "all 1s";
+    rec.innerHTML = `____________________________________________<br/>`
+    rec.innerHTML += slot[index].recommendation
+    element.addEventListener("click", ()=> message.innerHTML = slot[index].question);
+  }
+  if(index >=0 && index < 4)
+      container.style.backgroundColor = '#00BFFF'
+  else
+    if(index < 13)
+      container.style.backgroundColor = '#9400D3'
+    else
+      if(index < 15)
+        container.style.backgroundColor = '#191970'
+}
+function goToPrevious(){
+  rec.innerHTML= ''
+  message.innerHTML=''
+    if(index <= 0){
+      let last = slot.length - 1
+      //let restart = slot[index].angle + 24
+      img.style.transform = `rotate(-${slot[last].angle}deg)`;
+      img.style.transition = "all 1s";
+      index= 14
+      rec.innerHTML = `____________________________________________<br/>`
+      rec.innerHTML += slot[index].recommendation
+      element.addEventListener("click", ()=> message.innerHTML = slot[index].question);
+    }else {
+      index= index-1
+      img.style.transform = `rotate(-${slot[index].angle}deg)`;
+      img.style.transition = "all 1s";
+
+      rec.innerHTML = `____________________________________________<br/>`
+      rec.innerHTML += slot[index].recommendation
+      element.addEventListener("click", ()=> message.innerHTML = slot[index].question);
+    }
+  if(index >=0 && index < 4)
+      container.style.backgroundColor = '#00BFFF'
+  else
+    if(index < 13)
+      container.style.backgroundColor = '#9400D3'
+    else
+      if(index < 15)
+        container.style.backgroundColor = '#191970'
 }
